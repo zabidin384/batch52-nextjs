043cac73da3baa97f3c634a3bf7e8f56fee1ffad
zabidin384 <zabidin384@gmail.com>
2023-12-18 10:14:25 +0700

    Add tugas 6 - Menambahkan Metadata di NextJS

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from '@/layout'
 export default function Home({ children }) {
   return (
     <>
-      <Layout>
+      <Layout metaTitle="Home">
         <p>Home</p>
       </Layout>
     </>
--- pages/profile.js
+++ pages/profile.js
@@ -3,7 +3,7 @@ import Layout from '@/layout'
 export default function Profile({ children }) {
   return (
     <>
-      <Layout>
+      <Layout metaTitle="Profile" metaDescription="Semua informasi ini adalah seputar profile user">
         <p>Profile</p>
       </Layout>
     </>
