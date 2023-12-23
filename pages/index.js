a8f2b643cb1f6fa1e10a755c6e5bf23a3be72038
zabidin384 <zabidin384@gmail.com>
2023-12-23 09:00:44 +0700

    Hari 10 - Next Image dan Dynamic Import di NextJS

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,24 @@
-import Layout from "@/layout";
-import { useEffect } from "react";
+// import Layout from "@/layout";
+// import Image from "next/image";
+// import { useEffect } from "react";
+import dynamic from "next/dynamic";
+
+const LayoutComponent = dynamic(() => import("@/layout"));
 
 export default function Home({ children }) {
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((res) => console.log("response => ", res))
-      .catch((err) => console.log("err => ", err));
-  }, []);
+  // useEffect(() => {
+  //   fetch("/api/hello")
+  //     .then((res) => res.json())
+  //     .then((res) => console.log("response => ", res))
+  //     .catch((err) => console.log("err => ", err));
+  // }, []);
   return (
     <>
-      <Layout metaTitle="Home">
+      <LayoutComponent metaTitle="Home">
         <p>Home</p>
-      </Layout>
+        {/* <Image src="/logo.png" width={400} height={400} alt="logo S" />
+        <img src="/logo.png" style={{ width: 400, height: 400 }} alt="logo S" /> */}
+      </LayoutComponent>
     </>
   );
 }
