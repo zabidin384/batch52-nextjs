d6e3f73fbe8e0a544f9ef773d4b028d6f6e05e14
zabidin384 <zabidin384@gmail.com>
2023-12-23 08:49:54 +0700

    Hari 9 - Api Routes di NextJS

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
-import Layout from '@/layout'
+import Layout from "@/layout";
+import { useEffect } from "react";
 
 export default function Home({ children }) {
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((res) => console.log("response => ", res))
+      .catch((err) => console.log("err => ", err));
+  }, []);
   return (
     <>
       <Layout metaTitle="Home">
         <p>Home</p>
       </Layout>
     </>
-  )
+  );
 }
